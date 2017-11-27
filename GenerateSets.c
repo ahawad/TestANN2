04bcb9b7aaf746e3d3eccbd2c98b5f806c879bff
Ahmed Awad <b00042183@aus.edu>
2017-11-27 19:28:58 +0000

    Repository Cleanup

--- GenerateSets.c
+++ GenerateSets.c
@@ -1,37 +1,34 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() 
-{
-	FILE *training, *testing, *data;
-	data = fopen("datafile.txt", "w");
-	training = fopen("trainingfile.txt", "w");
-	//testing = fopen("testingfile.txt", "w");
+int main() {
+  FILE *training, *testing, *data;
+  data = fopen("datafile.txt", "w");
+  training = fopen("trainingfile.txt", "w");
+  // testing = fopen("testingfile.txt", "w");
 
-	if((data == NULL ) )
-	{
-		printf("ERROR \n");
-		exit(1);
-	}
+  if ((data == NULL)) {
+    printf("ERROR \n");
+    exit(1);
+  }
 
-	int traincounter, testcounter, linecounter, row; // trainingcounter, testing counter, counter for the file. 
-	traincounter = testcounter = linecounter = 1;
-	char buffer [200]; 
+  int traincounter, testcounter, linecounter,
+      row; // trainingcounter, testing counter, counter for the file.
+  traincounter = testcounter = linecounter = 1;
+  char buffer[200];
 
-	printf("%s", fgets(buffer, 200, data));
+  printf("%s", fgets(buffer, 200, data));
 
-/*	while(traincounter <= 80000)
-	{
-		row = (rand() % 100000) + 1;
+  /*	while(traincounter <= 80000)
+          {
+                  row = (rand() % 100000) + 1;
 
-		printf("%s", fgets(buffer, 200, data));
-		fprintf(training, "%s\n", buffer);
-		traincounter++;
-	}
-
-
-*/
+                  printf("%s", fgets(buffer, 200, data));
+                  fprintf(training, "%s\n", buffer);
+                  traincounter++;
+          }
 
 
+  */
 }
